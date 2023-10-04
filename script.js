66cf64d152e4f33011b29ff0a92362c11efba579
mohamedzeidan2021 <zeidan4mo@gmail.com>
2023-10-04 00:43:10 -0700

    Added more pages

---
+++ script.js
@@ -0,0 +1,14 @@
+// Add an event listener for the Contact button
+document.getElementById('contactButton').addEventListener('click', function() {
+    alert('You clicked the Contact button!');
+});
+
+// Add an event listener for the Github button
+document.getElementById('githubButton').addEventListener('click', function() {
+    alert('You clicked the Github button!');
+});
+
+// Add an event listener for the About button
+document.getElementById('aboutButton').addEventListener('click', function() {
+    alert('You clicked the About button!');
+});
